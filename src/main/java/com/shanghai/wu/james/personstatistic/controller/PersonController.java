21ab696a4587ebb1dce132c527a54921cb4c6a26
James Wu <jianwu3@paypal.com>
2023-01-07 23:48:44 +0800

    add page UI

--- src/main/java/com/shanghai/wu/james/personstatistic/controller/PersonController.java
+++ src/main/java/com/shanghai/wu/james/personstatistic/controller/PersonController.java
@@ -2,27 +2,33 @@ package com.shanghai.wu.james.personstatistic.controller;
 
 import com.shanghai.wu.james.personstatistic.model.PersonStat;
 import com.shanghai.wu.james.personstatistic.service.PersonService;
-import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person-stat")
 public class PersonController {
     @Resource
     private PersonService personService;
 
-    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    @RequestMapping(value = "/person-stat/update", method = RequestMethod.POST)
     public PersonStat update(@RequestBody PersonStat personStat) {
         personService.updatePersonStat(personStat);
         return personStat;
     }
 
-    @RequestMapping("list")
-    public List<PersonStat> list() {
-        return personService.listPersonStat();
+    @RequestMapping("/list")
+    public ModelAndView list() {
+        List<PersonStat> personStats = personService.listPersonStat();
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("personStats", personStats);
+        mv.setViewName("list.html");
+        return mv;
     }
 
 }
